123daa751af2d7f6bfb7acb6142790053e539fda
NandhiniKarvendhan <nandhukannan10@gmail.com>
2024-12-20 16:11:22 +0530

    Updating the page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/styles.css"; // Global styles
 import SMPPClientPage from "./pages/SMPPClientPage";
-import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/smpp-client" element={<SMPPClientPage />} />
+        <Route path="/" element={<SMPPClientPage />} />
       </Routes>
     </Router>
   );
