536a1dc6b44fbe4b7caac8db0060d199bd9ee492
NandhiniKarvendhan <nandhukannan10@gmail.com>
2025-01-04 16:43:49 +0530

    Updated with Link component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "./styles/LoginPage.css";
 import { auth } from "../config/firebaseConfig";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -43,11 +43,11 @@ const LoginPage = () => {
 
   const handleGoogleSignIn = async () => {
     const provider = new GoogleAuthProvider();
-  
+
     try {
       const result = await signInWithPopup(auth, provider);
       const idToken = await result.user.getIdToken();
-  
+
       const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/google-signin`, {
         method: "POST",
         headers: {
@@ -55,13 +55,13 @@ const LoginPage = () => {
         },
         body: JSON.stringify({ idToken }),
       });
-  
+
       if (!response.ok) {
         const errorData = await response.text(); // Log raw response to debug
         console.error("Error Response:", errorData);
         throw new Error("Google Sign-In failed");
       }
-  
+
       const data = await response.json();
       localStorage.setItem("token", data.token);
       navigate("/smpp-client");
@@ -70,7 +70,6 @@ const LoginPage = () => {
       alert("Google Sign-In failed. Please try again.");
     }
   };
-  
 
   return (
     <div className="login-page">
@@ -107,12 +106,12 @@ const LoginPage = () => {
             <button type="submit" className="login-button">Log in</button>
           </form>
           <div className="extra-links">
-            <a href="/reset" className="reset-link">Forgot your password? Reset</a>
+            <Link to="/reset" className="reset-link">Forgot your password? Reset</Link>
             <div className="separator">or</div>
             <button className="google-login" onClick={handleGoogleSignIn}>
               Sign in with Google
             </button>
-            <a href="/signup" className="signup-link">Don’t have an account? Sign up</a>
+            <Link to="/signup" className="signup-link">Don’t have an account? Sign up</Link>
           </div>
         </div>
       </div>
